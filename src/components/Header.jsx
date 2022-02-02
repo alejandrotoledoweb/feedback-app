9e98e948db37ca1795d73de43a58a1806c35336a
alejandro toledo <atoledofr@gmail.com>
2022-02-02 13:45:13 -0500

    add some elements to the header component and some base style in index.css file

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Header({ text }) {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <h2>Feedback UI</h2>
+          <p>{text}</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI',
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+);
