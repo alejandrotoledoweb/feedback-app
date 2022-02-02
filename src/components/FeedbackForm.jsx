7a85ea3b3d8f5a5a12c82475cd5e9cbef15ae9b5
alejandro toledo <atoledofr@gmail.com>
2022-02-02 17:48:11 -0500

    create shared component Button and apply it in the form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState();
@@ -17,7 +18,7 @@ function FeedbackForm() {
           placeholder="Write your review"
           value={text}
         />
-        <button type="submit">Submit</button>
+        <Button type="submit">Send</Button>
       </div>
     </Card>
   );
