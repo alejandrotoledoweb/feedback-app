2591ce4ba3c2ffc3304e553d3445f83dfdc0ca54
alejandro toledo <atoledofr@gmail.com>
2022-02-03 09:21:16 -0500

    add ReatingSelector component and added to the form with select func props

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelector from './RatingSelector';
 
 function FeedbackForm() {
   const [text, setText] = useState();
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
     setText(e.target.value);
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters long');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
   };
+
   return (
     <Card>
       <h2>How would you rate your service with us?</h2>
+      <RatingSelector select={(rating) => setRating(rating)} />
       <div className="input-group">
         <input
           type="text"
@@ -18,8 +34,11 @@ function FeedbackForm() {
           placeholder="Write your review"
           value={text}
         />
-        <Button type="submit">Send</Button>
+        <Button type="submit" isDisabled={btnDisabled}>
+          Send
+        </Button>
       </div>
+      {message && <div className="message">{message}</div>}
     </Card>
   );
 }
