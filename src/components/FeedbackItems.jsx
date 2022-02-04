0afb6ce5bf8b82f3a09492e95bc73ea42b2e7d50
alejandro toledo <atoledofr@gmail.com>
2022-02-04 09:27:24 -0500

    add the editFeedback function to pass the selected feedback to the global state

--- src/components/FeedbackItems.jsx
+++ src/components/FeedbackItems.jsx
@@ -1,17 +1,20 @@
 import { useContext } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackItems({ item }) {
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close" onClick={() => deleteFeedback(item.id)}>
         <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,6 +22,11 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
@@ -34,12 +39,20 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
